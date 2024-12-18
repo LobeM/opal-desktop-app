fac71b43aad95cf53402c688f85d7dee6ce2ab8d
Lobe <lo.musonda@pacra.org.zm>
2024-12-18 16:08:40 +0200

    Created Auth component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './App.css';
 import { Toaster } from 'sonner';
+import ControlLayout from './layouts/ControlLayout';
+import AuthButton from './components/global/AuthButton';
 
 const client = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={client}>
+      <ControlLayout>
+        <AuthButton />
+      </ControlLayout>
       <Toaster />
     </QueryClientProvider>
   );
