41ff41bef4d823482616d3854265d424ce5094f9
Lobe <lo.musonda@pacra.org.zm>
2024-12-19 16:42:28 +0200

    Added axios for HTTP requests, integrated vite-tsconfig-paths for improved path resolution, and implemented API proxying in Vite configuration. Updated App component to include Widget and enhanced utility functions for user profile fetching and media source retrieval.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,8 +1,33 @@
 import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
+import axios from 'axios';
+
+const httpClient = axios.create({
+  baseURL: import.meta.env.VITE_HOST_URL,
+});
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
 export const onCloseApp = () => window.ipcRenderer.send('closeApp');
+
+export const fetchUserProfile = async (clerkId: string) => {
+  const response = await httpClient.get(`/auth/${clerkId}`, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  return response.data;
+};
+
+export const getMediaSources = async () => {
+  const displays = await window.ipcRenderer.invoke('getSources');
+  const enumerateDevices =
+    await window.navigator.mediaDevices.enumerateDevices();
+  const audioInputs = enumerateDevices.filter(
+    (device) => device.kind === 'audioinput'
+  );
+  console.log('Getting sources');
+  return { displays, audio: audioInputs };
+};
